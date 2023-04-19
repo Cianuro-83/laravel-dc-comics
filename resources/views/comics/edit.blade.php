591b9003e1b45f434d3b58b52bde016139000375
Cianuro-83 <buzzeosimone@yahoo.com>
2023-04-19 16:13:35 +0200

    FEAT: add old to edit page

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -12,46 +12,46 @@
             <div class="row mb-3">
               <label for="title" class="col-sm-2 col-form-label">TITOLO</label>
               <div class="col-sm-10">
-                <input type="text" class="form-control" id="title" name="title" value="{{$comic->title}}">
+                <input type="text" class="form-control" id="title" name="title" value="{{ old('title',$comic->title)}}">
               </div>
             </div>
 
             <div class="row mb-3">
                 <label for="thumb" class="col-sm-2 col-form-label">THUMB</label>
                 <div class="col-sm-10">
-                  <input type="text" class="form-control" id="thumb" name="thumb" value="{{$comic->thumb}}">
+                  <input type="text" class="form-control" id="thumb" name="thumb" value="{{ old('thumb', $comic->thumb)}}">
                 </div>
               </div>
 
               <div class="row mb-3">
                 <label for="price" class="col-sm-2 col-form-label">PREZZO</label>
                 <div class="col-sm-10">
-                  <input type="text" class="form-control" id="price" name="price" value="{{$comic->price}}">
+                  <input type="text" class="form-control" id="price" name="price" value="{{ old('price', $comic->price)}}">
                 </div>
               </div>
   
               <div class="row mb-3">
                   <label for="series" class="col-sm-2 col-form-label">SERIE</label>
                   <div class="col-sm-10">
-                    <input type="text" class="form-control" id="series" name="series" value="{{$comic->series}}">
+                    <input type="text" class="form-control" id="series" name="series" value="{{ old('series', $comic->series)}}">
                   </div>
                 </div>
 
                 <div class="row mb-3">
                     <label for="sale-date" class="col-sm-2 col-form-label">DATA DI USCITA</label>
                     <div class="col-sm-10">
-                      <input type="text" class="form-control" id="sale-date" name="sale_date" value="{{$comic->sale_date}}">
+                      <input type="text" class="form-control" id="sale-date" name="sale_date" value="{{ old('sale_date', $comic->sale_date)}}">
                     </div>
                   </div>
       
                   <div class="row mb-3">
                       <label for="type" class="col-sm-2 col-form-label">TIPOLOGIA</label>
                       <div class="col-sm-10">
-                        <input type="text" class="form-control" id="type" name="type" value="{{$comic->type}}">
+                        <input type="text" class="form-control" id="type" name="type" value="{{ old('type', $comic->type)}}">
                       </div>
                     </div>
                     <div class="form-floating mb-3">
-                        <textarea class="form-control" placeholder="Leave a comment here" id="description" name="description"  style="height: 200px" >{{$comic->description}}
+                        <textarea class="form-control" placeholder="Leave a comment here" id="description" name="description"  style="height: 200px" >{{ old('description',$comic->description)}}
                         </textarea>
                         <label class="text-black" for="description">MODIFICA DESCRIZIONE
 
